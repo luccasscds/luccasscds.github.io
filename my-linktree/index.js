1afe975893db81c259fa7e2edb2e5615de6d40b7
luccasscds <lucassilvacds7@gmail.com>
2021-06-10 16:49:39 -0300

    f

--- my-linktree/index.js
+++ my-linktree/index.js
@@ -1,1 +1,18 @@
-console.log('ಠ_ಠ')
+console.log('ಠ_ಠ');//master
+
+const text = document.querySelector('.name-text');
+
+const efeitos = {
+    digitando(text) {
+        let textArray = text.innerText.split('');
+        text.innerHTML = "";
+
+        textArray.forEach((palavra,index) => {
+            setTimeout(function(){
+                text.innerHTML += palavra;
+            },700 * index)
+        })
+    }
+}
+
+efeitos.digitando(text);
