f3df5e3a3c450a2b8d0557de2ee5b21417b37c0b
luccasscds <lucassilvacds7@gmail.com>
2021-06-10 15:39:21 -0300

    efeito digitando no texto

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('ಠ_ಠ');
+
+const text = document.querySelector('.name-text');
+
+const efeitos = {
+    digitando(text) {
+        let textArray = text.innerText.split('');
+        text.innerHTML = "";
+
+        textArray.forEach((palavra,index) => {
+            setTimeout(function(){
+                text.innerHTML += palavra;
+            },700 * index)
+        })
+    }
+}
+
+efeitos.digitando(text);
