7ac0b6ae729a7ddda9e542d9b0f5b4cd10c8733e
luccasscds <lucassilvacds7@gmail.com>
2021-06-16 14:23:51 -0300

    removendo efeito do texto

--- my-linktree/index.js
+++ my-linktree/index.js
@@ -1,18 +1,1 @@
 console.log('ಠ_ಠ');
-
-const text = document.querySelector('.name-text');
-
-const efeitos = {
-    digitando(text) {
-        let textArray = text.innerText.split('');
-        text.innerHTML = "";
-
-        textArray.forEach((palavra,index) => {
-            setTimeout(function(){
-                text.innerHTML += palavra;
-            },700 * index)
-        })
-    }
-}
-
-efeitos.digitando(text);
