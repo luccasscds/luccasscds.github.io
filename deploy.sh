93483cd94b999432f77674c9e2e6b5459ffa6eb5
Lucas <lucas@unicorp.com.br>
2022-07-11 16:18:30 -0300

    arquivo: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 set -e
 
 yarn build
-yarn version --patch
+# yarn version --patch
 
 cd dist/
 
 git init
 git checkout -b main
 git add -A
-git commit --amend -m '🚀 Deploy'
+git commit -m '🚀 Deploy'
+# git commit --amend -m '🚀 Deploy'
 git push -f git@github.com:luccasscds/luccasscds.github.io.git production
 
 cd -
